ab7607ebb99ee97d7ba3e0829e7c680fd7382fc3
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-07-25 16:23:27 -0500

    adding reminaing stub routes

---
+++ views/places/index.jsx
@@ -0,0 +1,23 @@
+const React = require('react')
+const Def = require('./default')
+
+function index (data) {
+    let placesFormatted = data.places.map((place) => {
+        return (
+            <div>
+                <h2>{places.name}</h2>
+                <img src={place.pic} alt={place.name}/>
+            </div>
+        )
+    })
+    return (
+        <Def>
+            <main>
+                <h1>PLACES</h1>
+                {placesFormatted}
+            </main>
+        </Def>
+    )
+}
+
+module.exports = index
