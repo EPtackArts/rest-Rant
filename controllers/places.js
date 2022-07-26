1ce4f706bda62103b5ca0fe91ea0afde5dc4fe40
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-07-25 20:05:16 -0500

    adding styles to REST-rant

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,21 @@
+
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    let places = [{
+router.get ('/', (req, res) => {
+        let places = [{
         name: 'H-Thai-ML',
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
         pic: 'http://placekitten.com/250/250'
-    }, {
+      }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
         pic: 'http://placekitten.com/250/250'
-    }]
-    res.render('places/index', { places })
+      }]
+    res.render('places/index', {places})
 })
-  
+
 module.exports = router
