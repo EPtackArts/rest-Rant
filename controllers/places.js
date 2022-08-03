c64ba6f38088b2f308264e97c398e464d4a482ea
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-08-03 02:42:50 -0500

    show route actually connected for real now

--- controllers/places.js
+++ controllers/places.js
@@ -6,10 +6,6 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
   })
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
 router.post('/', (req, res) => {
   // console.log(req.body)
   if (!req.body.pic) {
@@ -26,16 +22,20 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-    res.render('error404')
+      res.render('error404')
   }
   else if (!places[id]) {
-    res.render('error404')
+      res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id]})
+      res.render('places/show', { place: places[id], id })
   }
 })
 
