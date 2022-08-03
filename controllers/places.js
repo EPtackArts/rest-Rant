9d06d7d62305458fc14b611465e57e868ff5bbfb
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-08-03 03:41:48 -0500

    working on PUT stub, but I'm going to sleep...

--- controllers/places.js
+++ controllers/places.js
@@ -6,22 +6,6 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
   })
 
-router.post('/', (req, res) => {
-  // console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = '/images/default.jpeg'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
@@ -39,16 +23,57 @@ router.get('/:id', (req, res) => {
   }
 })
 
-// router.put("/:id", (req, res) => {
-//   let id = Number(req.params.id)
-//   if (isNaN(id)) {
-//     res.render("error404");
-//   } else if (!places[id]) {
-//     res.render("error404")
-//   } else {
-//     res.redirect('places/show', { place: places[id], id })
-//   }
-// })
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/edit', {place: places[id]})
+  }
+})
+
+router.post('/', (req, res) => {
+  // console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = '/images/default.jpeg'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    if(!req.body.pic) {
+      req.body.pic ='/images/default.jpeg'
+    }
+    if(!req.body.city) {
+      req.body.city ='Anytown'
+    }
+    if(!req.body.state) {
+      req.body.state ='USA'
+    }
+    places[id] = req.body
+    res.redirect(`places/${id}`)
+  }
+})
 
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -64,17 +89,6 @@ router.delete('/:id', (req, res) => {
   }
 })
 
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/edit', {place: places[id]})
-  }
-})
+
 
 module.exports = router
