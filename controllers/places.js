d0c8b26e6c0b8a97c599314f196b71700c5627ea
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-08-10 18:26:04 -0500

    part 8 finished

--- controllers/places.js
+++ controllers/places.js
@@ -1,109 +1,172 @@
 const router = require('express').Router()
-const places = require("../models/places")
+const db = require('../models')
 
-// GET /places
 router.get('/', (req, res) => {
-    res.render('places/index', { places })
+  db.Place.find()
+  .then((places) => {
+    res.render('places/index', {places})
   })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
+})
+
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+    res.render('/places')
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
+})
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
 router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
+  db.Place.findById(req.params.id)
+  .then(place => {
+    res.render('places/show', {place})
+  })
+  .catch(err => {
+    console.log('err', err)
     res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
-})
-
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/edit', {place: places[id]})
-  }
-})
-
-router.post('/', (req, res) => {
-  // console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = '/images/default.jpeg'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
+  })
 })
 
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    if(!req.body.pic) {
-      req.body.pic ='/images/default.jpeg'
-    }
-    if(!req.body.city) {
-      req.body.city ='Anytown'
-    }
-    if(!req.body.state) {
-      req.body.state ='USA'
-    }
-    places[id] = req.body
-    res.redirect(`places/${id}`)
-  }
+  res.send('PUT /places/:id stub')
 })
 
 router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
+  res.send('DELETE /places/:id stub')
 })
 
-router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if(isNaN(id)) {
-    res.render('error404')
-  }
-  else if
-    (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
 })
 
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
 
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
 
 module.exports = router
+
+
+// const router = require('express').Router()
+// const places = require("../models/places")
+
+// // GET /places
+// router.get('/', (req, res) => {
+//     res.render('places/index', { places })
+//   })
+
+// router.get('/new', (req, res) => {
+//   res.render('places/new')
+// })
+
+// router.get('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     res.render('places/show', { place: places[id], id })
+//   }
+// })
+
+// router.get('/:id/edit', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     res.render('places/edit', {place: places[id]})
+//   }
+// })
+
+// router.post('/', (req, res) => {
+//   // console.log(req.body)
+//   if (!req.body.pic) {
+//     // Default image if one is not provided
+//     req.body.pic = '/images/default.jpeg'
+//   }
+//   if (!req.body.city) {
+//     req.body.city = 'Anytown'
+//   }
+//   if (!req.body.state) {
+//     req.body.state = 'USA'
+//   }
+//   places.push(req.body)
+//   res.redirect('/places')
+// })
+
+// router.put('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     if(!req.body.pic) {
+//       req.body.pic ='/images/default.jpeg'
+//     }
+//     if(!req.body.city) {
+//       req.body.city ='Anytown'
+//     }
+//     if(!req.body.state) {
+//       req.body.state ='USA'
+//     }
+//     places[id] = req.body
+//     res.redirect(`places/${id}`)
+//   }
+// })
+
+// router.delete('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     places.splice(id, 1)
+//     res.redirect('/places')
+//   }
+// })
+
+// router.delete('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if(isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if
+//     (!places[id]) {
+//       res.render('error404')
+//   }
+//   else {
+//     places.splice(id, 1)
+//     res.redirect('/places')
+//   }
+// })
+
+
+
+// module.exports = router
--- models/places.js
+++ models/places.js
@@ -1,19 +1,32 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai.jpg',
-    cite: 'https://unsplash.com/@grisskitchen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
-    photographer: 'Max Griss',
-    unsplash: "https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cafe.jpg',
-    cite: 'https://unsplash.com/es/@rebaspike?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
-    photographer:"Reba Spike",
-    unsplash: "https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-}]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+// module.exports = [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '/images/thai.jpg',
+//     cite: 'https://unsplash.com/@grisskitchen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+//     photographer: 'Max Griss',
+//     unsplash: "https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+// }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/cafe.jpg',
+//     cite: 'https://unsplash.com/es/@rebaspike?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+//     photographer:"Reba Spike",
+//     unsplash: "https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+// }]
